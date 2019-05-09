246c7154b111ee600cfa72fd76236a298735ff67
Eden Yeung <basktballer@gmail.com>
2019-05-09 15:39:36 +0000

    ChatBar functionality

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,13 +1,31 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+
+  onKeyPressMessage = (e) => {
+    let message = e.target.value;
+    if(e.which === 13) {
+      this.props.sendNewMessage(message);
+      e.target.value = "";
+    }
+  }
+
+  onKeyPressUsername = (e) => {
+    let username = e.target.value;
+    if(e.which === 13) {
+      this.props.changeUsernameValue(username)
+    }
+  }
+  
   render() {
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-username" name="username" defaultValue={this.props.currentUserName} onKeyPress={this.onKeyPressUsername} />
+        <input className="chatbar-message" name="content" placeholder="Type a message and hit ENTER" onKeyPress={this.onKeyPressMessage}/>
     </footer>
     );
   }
+
+
 }
 export default ChatBar;
